224081343f2b89fe12b3fedce2567adbae9f8ef8
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2025-11-13 14:00:53 +0000

    feature #3: replaced print with logger

--- app/tasks/google_map_scraper.py
+++ app/tasks/google_map_scraper.py
@@ -1,17 +1,16 @@
-import asyncio
-import os
+from app.core import Logger
 from app.core.data_exporters import FileSaver
 from app.google_map.scraper import scraper
 from app.celery import celery_app
 
-
+logger = Logger.get_logger(__file__,'google_map')
 @celery_app.task(bind=True,name="map scraper")
 async def run_scraper(self,query: str, file_name: str):
     try:
-        print(f"[INFO] Starting scrape for: {query}")
+        logger.info(f"Starting scrape for: {query}")
         data = await scraper(query)
         FileSaver.save(data, f"{file_name}")
-        print(f"[SUCCESS] Saved results to {file_name}")
+        logger.info(f"[SUCCESS] Saved results to {file_name}")
         return data
     except Exception as e:
         self.retry(exc=e, countdown=5, max_retries=3)
